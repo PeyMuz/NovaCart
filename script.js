91b2b381fc5d0fb540dd548d121a3aba10eb3311
Kylle Bantog <kbantog1@gmail.com>
2026-05-28 11:08:19 +0800

    Product details done

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ function menuToggle(){
         menuitems.style.padding = "10px 30px 20px";
     }
 }
+
+// ---------------------Js for product gallery---------------------
+
+let productImg = document.getElementById("productImg");
+let smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function(){
+    productImg.src = smallImg[0].src;
+    
+}
+smallImg[1].onclick = function(){
+    productImg.src = smallImg[1].src;
+    
+}
+smallImg[2].onclick = function(){
+    productImg.src = smallImg[2].src;
+    
+}
+smallImg[3].onclick = function(){
+    productImg.src = smallImg[3].src;
+    
+}
